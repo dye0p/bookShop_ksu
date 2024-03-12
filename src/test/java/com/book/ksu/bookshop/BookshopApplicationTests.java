4e9e31a9bacd1391debb0cb52cd5e82da307e81f
DongYeop <s01054830455@gmail.com>
2024-03-12 18:06:39 +0900

    프로젝트 생성

---
+++ src/test/java/com/book/ksu/bookshop/BookshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.ksu.bookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
