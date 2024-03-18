e73d5025691d3e9df155f33db94a831aa41af465
DongYeop <s01054830455@gmail.com>
2024-03-18 18:14:47 +0900

    홈 컨트롤러

---
+++ src/main/java/com/book/ksu/bookshop/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.book.ksu.bookshop.controller;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String homeP(Model model) {
+
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        model.addAttribute("username", username); //사용자 이름
+
+        return "home";
+    }
+}
