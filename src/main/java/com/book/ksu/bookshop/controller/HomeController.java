0aff94e6d36cfa89d50af0d93e40113af990cd8f
DongYeop <s01054830455@gmail.com>
2024-03-18 18:58:20 +0900

    메인화면 컨트롤러

--- src/main/java/com/book/ksu/bookshop/controller/HomeController.java
+++ src/main/java/com/book/ksu/bookshop/controller/HomeController.java
@@ -10,6 +10,7 @@ public class HomeController {
     @GetMapping("/")
     public String homeP(Model model) {
 
+        //사용자 이름
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
 
         model.addAttribute("username", username); //사용자 이름
